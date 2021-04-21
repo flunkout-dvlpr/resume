0ffb2c38d56334dc3cdd2c760d00423f63afb8e4
flunkout-dvlpr <info@juliogonzalez.space>
2021-04-21 15:03:49 -0500

    Add profileImage prop, update buttons size

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,14 +3,29 @@
     <q-header elevated>
       <q-toolbar class="q-ma-xs row justify-center">
         <div class="col-12 justify-center">
-            <div class="row justify-center text-center text-h4">Julio C. Gonzalez</div>
-            <div class="row justify-center text-center text-h10">Software Developer</div>
+            <div class="row justify-center">
+              <img
+                :src="profileImage"
+                style="width: 125px; height: 125px; border-radius: 50%; border: 5px solid #a1a1a1;"
+              >
+            </div>
+            <div class="row justify-center text-h6">Julio C. Gonzalez</div>
+            <div class="row justify-center text-subtiitle1">Full Stack Software Developer</div>
+            <div class="row justify-center text-caption">Houston, TX</div>
         </div>
       </q-toolbar>
+
+      <q-tabs
+        dense
+        class="bg-secondary text-black">
+        <q-route-tab to="/page1" no-caps label="About Me" />
+        <q-route-tab to="/page2" no-caps label="Experience" />
+        <q-route-tab to="/page3" no-caps label="Projects" />
+      </q-tabs>
     </q-header>
 
     <q-footer elevated>
-      <q-toolbar class="q-ma-xs row justify-center">
+      <q-toolbar class="q-mx-xs row justify-center">
         <div class="col-xs-3 col-sm-3 col-md-1 col-lg-1 col-xl-1">
           <q-btn
             flat
@@ -18,7 +33,7 @@
             type="a"
             target="_blank"
             icon="ion-logo-github"
-            size="lg"
+            size="md"
             href="https://github.com/flunkout-dvlpr"
           />
         </div>
@@ -29,7 +44,7 @@
             type="a"
             target="_blank"
             icon="ion-logo-linkedin"
-            size="lg"
+            size="md"
             href="https://www.linkedin.com/in/julio-gonzalez-4b2904b0"
           />
         </div>
@@ -39,7 +54,7 @@
             class="fit"
             type="a"
             icon="phone"
-            size="lg"
+            size="md"
             href="tel:832-589-5877"
           />
         </div>
@@ -50,7 +65,7 @@
             type="a"
             target="_blank"
             icon="mail"
-            size="lg"
+            size="md"
             href="mailto:info@juliogonzalez.space"
           />
         </div>
@@ -66,6 +81,11 @@
 
 <script>
 export default {
-  name: 'MainLayout'
+  name: 'MainLayout',
+  data () {
+    return {
+      profileImage: 'https://media-exp1.licdn.com/dms/image/C4E03AQH06TWHQ2tZ_w/profile-displayphoto-shrink_400_400/0/1565450126632?e=1624492800&v=beta&t=M2kv1H-Qewc0ZOzBPBHdewWQPxVf8E9PFNBxQc6IoHQ'
+    }
+  }
 }
 </script>
