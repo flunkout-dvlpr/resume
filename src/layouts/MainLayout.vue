de64ede34de99dff4d12f5c1eb9ff99b47442e19
flunkout-dvlpr <info@juliogonzalez.space>
2022-01-31 10:08:19 -0500

    Restructure toolbar, abstract data to props

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,36 +1,46 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
-      <q-toolbar class="q-ma-xs row justify-center">
-        <div class="col-12 justify-center">
-            <div class="row justify-center">
-              <q-btn
-                round
-                no-ripple
-                @click="switchImage = !switchImage"
+      <q-toolbar class="launchit-gradient q-pa-none q-ma-none">
+        <div class="q-py-sm fit row justify-center items-center content-center">
+          <div class="q-pb-xs col-12 column justify-center items-center content-center">
+            <q-btn
+              round
+              no-ripple
+              @click="switchImage = !switchImage"
+            >
+              <q-img
+                transition="flip-left"
+                :src="switchImage ? images.profile : images.avatar"
+                :style="`width: 125px; height: 125px; border-radius: 50%; border: 5px solid ${switchImage ? '#303030' : '#ebebeb'}`"
               >
-                <q-img
-                  transition="flip-left"
-                  :src="switchImage ? images.profile : images.avatar"
-                  style="width: 125px; height: 125px; border-radius: 50%; border: 5px solid #a1a1a1;"
-                />
-              </q-btn>
-            </div>
-            <div class="row justify-center text-h6">Julio C. Gonzalez</div>
-            <div class="row justify-center text-subtiitle1">Full Stack Dev - AWS Architect</div>
-            <div class="row justify-center text-caption">Houston, TX / Miami, FL</div>
+              </q-img>
+            </q-btn>
+          </div>
+          <div class="q-pb-xs col-12 column justify-center items-center content-center">
+            <div class="text-center text-h6">{{profile.fullName}}</div>
+          </div>
+        <div class="q-pb-xs col-12 column justify-center items-center content-center">
+          <div class="text-center text-subtiitle1">{{profile.position}}</div>
+        </div>
+        <div class="q-pb-none col-12 column justify-center items-center content-center">
+          <div class="text-center text-caption">{{profile.caption}}</div>
+        </div>
+        <div class="q-pb-xs col-12 column justify-center items-center content-center">
+          <div class="text-center text-caption">{{profile.location}}</div>
+        </div>
         </div>
       </q-toolbar>
-
       <q-tabs
         dense
-        class="bg-secondary text-black"
         v-model="section"
+        indicator-color="accent"
+        active-color="secondary"
         @input="scrollToSection"
       >
-        <q-tab no-caps name="about-me" label="About Me" />
-        <q-tab no-caps name="experience" label="Experience" />
-        <q-tab no-caps name="projects" label="Projects" />
+        <q-tab class="col-2" no-caps name="about-me" label="About Me" />
+        <q-tab class="col-2" no-caps name="experience" label="Experience" />
+        <q-tab class="col-2" no-caps name="projects" label="Projects" />
       </q-tabs>
     </q-header>
 
@@ -44,7 +54,7 @@
             target="_blank"
             icon="ion-logo-github"
             size="md"
-            href="https://github.com/flunkout-dvlpr"
+            :href="links.github"
           />
         </div>
         <div class="col-xs-3 col-sm-3 col-md-1 col-lg-1 col-xl-1">
@@ -55,7 +65,7 @@
             target="_blank"
             icon="ion-logo-linkedin"
             size="md"
-            href="https://www.linkedin.com/in/julio-gonzalez-4b2904b0"
+            :href="links.linkedin"
           />
         </div>
         <div class="col-xs-3 col-sm-3 col-md-1 col-lg-1 col-xl-1">
@@ -65,7 +75,7 @@
             type="a"
             icon="phone"
             size="md"
-            href="tel:832-589-5877"
+            :href="`tel:${links.phone}`"
           />
         </div>
         <div class="col-xs-3 col-sm-3 col-md-1 col-lg-1 col-xl-1">
@@ -76,14 +86,14 @@
             target="_blank"
             icon="mail"
             size="md"
-            href="mailto:info@juliogonzalez.space"
+            :href="`mailto:${links.email}`"
           />
         </div>
       </q-toolbar>
     </q-footer>
 
     <q-page-container>
-      <router-view />
+      <router-view/>
     </q-page-container>
 
   </q-layout>
@@ -96,6 +106,19 @@ export default {
     return {
       section: 'about-me',
       switchImage: false,
+      profile: {
+        fullName: 'Julio C. Gonzalez',
+        position: 'Full Stack Dev | Solutions Architect',
+        caption: 'Hablo Español!',
+        location: 'Houston, TX | Miami, FL'
+
+      },
+      links: {
+        github: 'https://github.com/flunkout-dvlpr',
+        linkedin: 'https://www.linkedin.com/in/julio-gonzalez-4b2904b0',
+        phone: '832-589-5877',
+        email: 'info@juliogonzalez.space'
+      },
       images: {
         profile: 'profile.jpeg',
         avatar: 'avatar.png'
@@ -104,10 +127,11 @@ export default {
   },
   methods: {
     scrollToSection (section) {
-      console.log(section)
       const element = document.getElementById(section)
-      const y = element.getBoundingClientRect().top + window.pageYOffset - 250
-      window.scrollTo({ top: y, behavior: 'smooth' })
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' })
+      // const scrollWindow = document.getElementById('scroll-section')
+      // const y = element.getBoundingClientRect().top
+      // scrollWindow.scrollTo({ top: y, behavior: 'smooth' })
       this.$router.push({ path: `/${section}` })
       if (section === 'projects') {
         this.switchImage = false
